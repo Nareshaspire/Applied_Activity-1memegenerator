2b1828d311bbd2a536e0baf7e3255d198591d4dc
nareshaspire <Nareshaspire45@gmail.com>
2022-11-08 12:45:15 -0500

    mem generator

---
+++ memegenerator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace memegenerator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ memegenerator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace memegenerator;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
